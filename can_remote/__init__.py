9bc5e3d869aafcdd715181211db96453a485b5dd
Christian Sandberg <christiansandberg@me.com>
2017-12-10 14:14:27 +0100

    Create setup.py

--- can_remote/__init__.py
+++ can_remote/__init__.py
@@ -4,3 +4,4 @@ DEFAULT_PORT = 54701
 from .client import RemoteBus, CyclicSendTask
 from .server import RemoteServer
 from .protocol import RemoteError
+from .version import __version__
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+exec(open("can_remote/version.py").read())
+
+description = open("README.rst").read()
+
+setup(
+    name="python-can-remote",
+    url="https://github.com/christiansandberg/python-can-remote",
+    version=__version__,
+    packages=find_packages(),
+    author="Christian Sandberg",
+    author_email="christiansandberg@me.com",
+    description="CAN over network bridge for Python",
+    keywords="CAN TCP websocket",
+    long_description=description,
+    license="MIT",
+    platforms=["any"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering"
+    ],
+    install_requires=["python-can"]
+)
